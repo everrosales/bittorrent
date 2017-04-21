7f5b5858ecaa06b4e7322a233821244434cd6a9d
Nicole Glabinski <nicolekg@mit.edu>
2017-04-21 16:52:35 -0400

    Adding create and kill funcs for client

--- src/client/client.go
+++ src/client/client.go
@@ -1,1 +1,27 @@
+package btclient
 //Client
+
+import "sync"
+
+type BTClient struct {
+    mu sync.Mutex
+    shutdown chan bool
+}
+
+func StartBTClient() *BTClient {
+    client := &BTClient{}
+    rf.shutdown = make(chan bool)
+
+    go client.main()
+    return client
+}
+
+func (cl *BTClient) Kill() {
+    close(cl.shutdown)
+}
+
+func (cl *BTClient) main() {
+    for {
+        time.Sleep(10 * time.Millisecond)
+    }
+}
